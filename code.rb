31a87ff27ea355a8aa5467fff00a812d8aa6e2ee
SasquatchShawn <srmiranda1@gmail.com>
2015-08-24 13:17:22 -0400

    added new way to parse csv

--- code.rb
+++ code.rb
@@ -1,5 +1,6 @@
 #YOUR CODE GOES HERE
 require "pg"
+require "csv"
 
 def db_connection
   begin
@@ -14,8 +15,16 @@ end
 #   conn.exec("COPY ingredients FROM '/Users/Sasquatch/challenges/evening-csv-to-db/ingredients.csv' DELIMITER ',' CSV;")
 # end
 
+# # D-Rods Way
+CSV.foreach("ingredients.csv", headers: true) do |row|
+  db_connection do |conn|
+    conn.exec_params("INSERT INTO ingredients (number, ingredient) VALUES ($1, $2)", [row["number"], row["ingredient"]])
+  end
+end
+
+
 db_connection do |conn|
-  results = conn.exec('SELECT number, ingredient FROM ingredients;')
+  results = conn.exec('SELECT * FROM ingredients;')
   results.each do |list|
     puts "#{list['number']}. #{list['ingredient']}"
   end
